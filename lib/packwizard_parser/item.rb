1542123a32c43cc1456c21cce720d103db98abb4
Haimen <haimenabbas@gmail.com>
2026-04-02 13:46:10 +0200

    feat: Create List, Category and Item classes

---
+++ lib/packwizard_parser/item.rb
@@ -0,0 +1,50 @@
+# frozen_string_literal: true
+
+module PackwizardParser
+
+  class Item
+    attr_reader :name, :description, :weight, :total_weight, :quantity, :price, :total_price, :image_url,
+                :consumable, :total_consumable_weight, :worn, :worn_quantity, :total_worn_weight
+
+    def initialize(name:, weight:, total_weight:, quantity:, description: nil,
+                   image_url:nil, consumable: false, total_consumable_weight: nil,
+                   worn: false, worn_quantity: nil, total_worn_weight: nil)
+
+      @name = name
+      @weight = weight
+      @description = description
+      @total_weight = total_weight
+      @quantity = quantity
+      @image_url = image_url
+      @consumable = consumable
+      @total_consumable_weight = total_consumable_weight
+      @worn = worn
+      @worn_quantity = worn_quantity
+      @total_worn_weight = total_worn_weight
+    end
+
+    alias worn? worn
+
+    alias consumable? consumable
+
+    # Convert to hash
+    #
+    # @return [Hash] Hash representation of the item
+    def to_h
+      {
+        name: name,
+        description: description,
+        weight: weight,
+        total_weight: total_weight,
+        quantity: quantity,
+        image_url: image_url,
+        consumable: consumable,
+        total_consumable_weight: total_consumable_weight,
+        worn: worn,
+        worn_quantity: worn_quantity,
+        total_worn_weight: total_worn_weight
+      }
+    end
+
+  end
+end
