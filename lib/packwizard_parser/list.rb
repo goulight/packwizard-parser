1cf1b3ef3edc0baf1e4c98796128ca3b3daf751c
Haimen <haimenabbas@gmail.com>
2026-04-07 15:04:27 +0200

    chore: Added documentation to model classes.

--- lib/packwizard_parser/list.rb
+++ lib/packwizard_parser/list.rb
@@ -1,15 +1,25 @@
 # frozen_string_literal: true
 
 module PackwizardParser
+  # Represents a PackWizard list.
+  #
+  # @attr_reader [String] name The name of the list
+  # @attr_reader [String, nil] description Optional description of the list
+  # @attr_reader [Array<Category>] categories Array of categories in this list
   class List
     attr_reader :name, :description, :categories
 
+    # @param name [String] The name of the list
+    # @param description [String, nil] Optional description
+    # @param categories [Array<Category>] Array of categories in this list
     def initialize(name:, description: nil, categories: [])
       @name = name
       @description = description
       @categories = categories
     end
 
+    # Convert to hash for backward compatibility
+    # @return [Hash] Hash representation of the list
     def to_h
       {
         name: name,
