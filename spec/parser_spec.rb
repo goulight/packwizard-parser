1c5c347234d0985bef7da34375290f7a8bd36a6b
Haimen <haimenabbas@gmail.com>
2026-04-08 11:50:40 +0200

    test: testcases for Parser class.

--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -2,10 +2,51 @@
 
 RSpec.describe PackwizardParser::Parser do
   let(:fixture_json) { File.read(File.join(__dir__, 'fixtures', 'tUE6BJs.json')) }
-  let(:data) { JSON.parse(fixture_json) }
-  let(:rows) { data['tableData'].first['rows'] }
-  # let(:parser) { described_class.new}
+  let(:fake_response) do
+    instance_double(HTTParty::Response, success?: true, body: fixture_json, code: 200)
+  end
 
-  # TODO
-  # Integration tests for parser.rb
+  before do
+    allow(HTTParty).to receive(:get).and_return(fake_response)
+  end
+
+  describe "#parse" do
+    subject(:list) { described_class.new(shareable_id: 'tUE6BJs').parse }
+
+    it 'returns a List object' do
+      expect(list).to be_a(PackwizardParser::List)
+    end
+
+    it 'extracts the list name' do
+      expect(list.name).to eq('Ultralight Gear List 2023')
+    end
+
+    it 'extracts the list description' do
+      expect(list.description).to end_with('but this pack comes close!')
+    end
+
+    it 'accepts a full URL and extracts the ID' do
+      parser = described_class.new(shareable_id: 'https://www.packwizard.com/s/tUE6BJs')
+      parser.parse
+      expect(HTTParty).to have_received(:get).with(
+        PackwizardParser::Parser::API_URL,
+        hash_including(query: { shareableId: 'tUE6BJs' }),
+      )
+    end
+
+    it 'calls the API with the correct shareable ID' do
+      list
+      expect(HTTParty).to have_received(:get).with(
+        PackwizardParser::Parser::API_URL,
+        hash_including(query: { shareableId: 'tUE6BJs' })
+      )
+    end
+
+    it 'raises a failed response' do
+      allow(HTTParty).to receive(:get).and_return(
+        instance_double(HTTParty::Response, success?: false, code: 404),
+      )
+      expect { described_class.new(shareable_id: "bad").parse }.to raise_error(RuntimeError, /404/)
+    end
+  end
 end
